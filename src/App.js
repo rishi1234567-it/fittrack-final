17ed21784455e0d7fed21b390b6d0b1756a4d8c3
rishi1234567-it <rishiselvam179@gmail.com>
2025-06-10 14:06:47 +0530

    Initial FitTrack Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import WorkoutSelector from './components/WorkoutSelector';
+import DailyProgress from './components/DailyProgress';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>🏋️ FitTrack - Your Daily Workout Tracker</h1>
+      <WorkoutSelector />
+      <hr />
+      <DailyProgress />
     </div>
   );
 }
